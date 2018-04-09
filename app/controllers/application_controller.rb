301a548c51df4a346b18a08af7fa47c706ee06c5
Paul Cosgrove <pacosgrove1@gmail.com>
2018-04-09 11:38:48 +0100

    [thredded_create_app] Configure Devise to support display_name in forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i(display_name))
+    devise_parameter_sanitizer.permit(:account_update, keys: %i(display_name))
+  end
 
   before_action :store_current_location, unless: :devise_controller?
   helper_method :back_url
