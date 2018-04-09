2359232c62fd90e77b4fe3b05385faba2de0aa83
Paul Cosgrove <pacosgrove1@gmail.com>
2018-04-09 11:38:24 +0100

    [thredded_create_app] Setup Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :store_current_location, unless: :devise_controller?
+  helper_method :back_url
+
+  private
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
+  def after_sign_out_path_for(resource)
+    stored_location_for(:user) || (respond_to?(:root_path) ? root_path : thredded.root_path)
+  end
+
+  def back_url
+    session[:user_return_to] || (respond_to?(:root_path) ? root_path : thredded.root_path)
+  end
 end
