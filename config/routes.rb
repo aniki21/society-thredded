3c08005d9ac75a033e1fd98f937a1747a6680877
Paul Cosgrove <pacosgrove1@gmail.com>
2018-04-09 11:38:56 +0100

    [thredded_create_app] Set up basic app navigation and styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do  
+  root to: 'home#show'
   scope path: 'admin' do
     authenticate :user, lambda { |u| u.admin? } do
       mount RailsEmailPreview::Engine, at: 'emails'
@@ -8,5 +9,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
   mount Thredded::Engine => '/forum'
 end
