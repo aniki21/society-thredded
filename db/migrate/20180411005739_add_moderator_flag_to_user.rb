6b1208f850d984e904b3f5234d58162a4bc4adc3
Paul Cosgrove <pacosgrove1@gmail.com>
2018-04-11 02:04:27 +0100

    add moderator flag

---
+++ db/migrate/20180411005739_add_moderator_flag_to_user.rb
@@ -0,0 +1,5 @@
+class AddModeratorFlagToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :moderator, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409103847) do
+ActiveRecord::Schema.define(version: 20180411005739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -266,6 +266,7 @@ ActiveRecord::Schema.define(version: 20180409103847) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
     t.string "display_name", null: false
+    t.boolean "moderator", default: false
     t.index "lower((display_name)::text) text_pattern_ops", name: "users_display_name_lower", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
