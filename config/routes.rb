0fc208f8e6f1aaee867b2207d39ba360f2b8c30f
Paul Cosgrove <pacosgrove1@gmail.com>
2018-04-11 11:20:54 +0100

    mount forum at root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do  
-  root to: redirect('/forum')
+  # root to: redirect('/forum')
   scope path: 'admin' do
     authenticate :user, lambda { |u| u.admin? } do
       mount RailsEmailPreview::Engine, at: 'emails'
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
-  mount Thredded::Engine => '/forum'
+  mount Thredded::Engine => '/'
 end
