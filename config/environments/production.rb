e9dc0f6d63c5462f938d8798cb8021709cbcbf28
Paul Cosgrove <pacosgrove1@gmail.com>
2018-04-11 14:51:36 +0100

    mail domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    domain: Settings.hostname,
+    domain: ENV['MAILGUN_DOMAIN'],
     port: ENV['MAILGUN_SMTP_PORT'],
     address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
