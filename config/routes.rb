e271e75cd64cb227689f6fb00835887b26d496cf
Paul Cosgrove <pacosgrove1@googlemail.com>
2018-05-24 17:47:10 +0100

    reorder network icons alphabetically, add PM link

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show]
+
+  resources :private_topics, only: [:new], controller: 'thredded/private_topics'
   mount Thredded::Engine => '/'
 end
