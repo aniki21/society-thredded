21054b6f73de7b5d8c40878b255af91dabfdb573
Gleb Mazovetskiy <glex.spb@gmail.com>
2018-12-05 21:52:26 +0000

    fix moderator checkbox

--- app/controllers/thredded_ext/moderation/users_controller.rb
+++ app/controllers/thredded_ext/moderation/users_controller.rb
@@ -6,8 +6,7 @@ module ThreddedExt
       def update
         return head(:unauthorized) unless current_user.admin?
         user = Thredded.user_class.find(params[:id])
-        user[Thredded.moderator_column] = params.require(:user).permit(:moderator)
-        user.save
+        user.update(params.require(:user).permit(:moderator))
         redirect_back fallback_location: thredded.user_moderation_path(user.id)
       end
     end
